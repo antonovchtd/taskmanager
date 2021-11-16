e89ba17f7d8fe073d74c93c018b27a60e8a92910
Anton Ovcharenko <antonovch@icloud.com>
2021-11-16 17:57:56 +0200

    moved out TaskID and IDGenerator classes

--- src/model/TaskManager.h
+++ src/model/TaskManager.h
@@ -10,35 +10,19 @@
 #include <map>
 
 #include "Task.h"
-
-class TaskID{
-public:
-    explicit TaskID(unsigned int val) : value_(val) { };
-    unsigned int value() const { return value_; };
-    bool operator==(const TaskID&) const;
-    bool operator<(const TaskID&) const;
-private:
-    unsigned int value_;
-};
-
-class IDGenerator{
-public:
-    IDGenerator() : last_(0) {};
-    explicit IDGenerator(unsigned int last) : last_(last) {};
-    TaskID genID(){ return TaskID(last_++); }
-private:
-    unsigned int last_;
-};
+#include "TaskID.h"
 
 class TaskManager {
 public:
-    TaskID Add(Task t);
-    void Edit(TaskID, Task t);
+    TaskID Add(Task);
+    void Edit(TaskID, Task);
     void Complete(TaskID);
     void Delete(TaskID);
-    std::map<TaskID, Task> getTasks();
-    void Show(std::ostream &os);
+    void Show(std::ostream &os) const;
+
+    std::map<TaskID, Task> getTasks() const;
     bool Validate(TaskID id) const;
+
 private:
     std::map<TaskID, Task> tasks_;
     IDGenerator gen_;
