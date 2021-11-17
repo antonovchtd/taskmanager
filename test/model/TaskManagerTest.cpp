cc1661df01b930e1b26ffd7c333dea030eb4a72b
Anton Ovcharenko <anton.ovcharenko@teamdev.com>
2021-11-17 20:56:34 +0200

    first mock test

--- test/model/TaskManagerTest.cpp
+++ test/model/TaskManagerTest.cpp
@@ -6,14 +6,25 @@
 // Created by Anton O. on 11/12/21.
 //
 
-#include "../../src/model/Task.h"
 #include "../../src/model/TaskManager.h"
 
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using ::testing::AtLeast;
+using ::testing::Return;
 
 class TaskManagerTest : public ::testing::Test
 {
 
+};
+
+class MockIDGenerator : public IDGenerator
+{
+public:
+    MockIDGenerator(unsigned int last) {last_ = last;};
+    MockIDGenerator() {last_ = 0;};
+    MOCK_METHOD(TaskID, genID, (), (override));
 };
 
 TEST_F(TaskManagerTest, shouldAddTask)
@@ -48,4 +59,22 @@ TEST_F(TaskManagerTest, shouldCompleteTask)
     tm.Complete(id);
     auto tasks = tm.getTasks();
     EXPECT_TRUE(tasks.find(id)->second.isComplete());
+}
+
+TEST_F(TaskManagerTest, shouldAddMultipleTasks)
+{
+    std::shared_ptr<MockIDGenerator> generator = std::shared_ptr<MockIDGenerator>(new MockIDGenerator);
+    EXPECT_CALL(*generator, genID())
+        .Times(3)
+        .WillOnce(Return(TaskID(1)))
+        .WillOnce(Return(TaskID(2)))
+        .WillOnce(Return(TaskID(3)));
+
+    TaskManager tm(generator);
+    Task t1 = Task::Create("My Test Task 1", Task::Priority::MEDIUM, "30/11/2021 19:00");
+    Task t2 = Task::Create("My Test Task 2", Task::Priority::MEDIUM, "in 02:00:00");
+    TaskID id1 = tm.Add(t1);
+    TaskID id2 = tm.Add(t2);
+    TaskID id3 = tm.Add(t2);
+    EXPECT_EQ(3,tm.size());
 }
