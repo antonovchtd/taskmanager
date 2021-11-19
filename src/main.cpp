eb26b35ddad389a020dca9c25f523ffc768061e9
Anton Ovcharenko <antonovch@icloud.com>
2021-11-19 23:05:09 +0200

    added user loop

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,21 @@
 
 #include "model/Task.h"
 #include "model/TaskManager.h"
+#include "model/Prompt.h"
 
 int main() {
     TaskManager tm;
-    TaskID id1 = tm.Add(Task::Create("My First Task",Task::Priority::HIGH,"in 01:10:05"));
-    TaskID id2 = tm.Add(Task::Create("Second Task","16/11/21 19:00"));
-    TaskID id12 = tm.Add(Task::Create("First Task's subtask","in 02:00:00"), id1);
+    Task::Priority p = Task::Priority::NONE;
+    TaskID id1 = tm.Add(Task::Create("My First Task",Task::Priority::HIGH,"in 01:10:05",false));
+    TaskID id2 = tm.Add(Task::Create("Second Task", p, "16/11/21 19:00", false));
+    TaskID id12 = tm.Add(Task::Create("First Task's subtask", p,"in 02:00:00", false), id1);
+    TaskID id22 = tm.Add(Task::Create("Second Task's subtask", p,"16/11/21 19:00", false), id2);
+    TaskID id23 = tm.Add(Task::Create("Second Task's subsubtask", p, "16/11/21 19:00", false), id22);
 
     std::cout << "First call ------\n";
     tm.Show(std::cout);
 
-    tm.Edit(id2, Task::Create("Second Task edited",Task::Priority::MEDIUM, -1));
+    tm.Edit(id2, Task::Create("Second Task edited",Task::Priority::MEDIUM, time(nullptr)-1, false));
     std::cout << "Second call ------\n";
     tm.Show(std::cout);
 
@@ -37,9 +41,20 @@ int main() {
     tm.Show(std::cout);
 
     tm.Delete(id1);
-    tm.Add(Task::Create("The Third Task",Task::Priority::LOW));
+    tm.Add(Task::Create("The Third Task",static_cast<Task::Priority>(1), "in 00:30", false));
     std::cout << "Fourth call ------\n";
     tm.Show(std::cout);
+
+    std::string s = std::string{"0"};
+    std::cout << std::atoi(s.c_str()) << std::endl;
+
+    Prompt prompt;
+    TaskManager tm2;
+    while (!prompt.quit_flag()){
+        prompt.ask(std::cout);
+        prompt.input(std::cout, std::cin);
+        prompt.act(std::cout,tm2);
+    }
 
     return 0;
 }
