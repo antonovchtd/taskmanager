938f51c677ef7138421077630618a860463e63d2
Anton Ovcharenko <antonovch@icloud.com>
2021-11-24 12:04:05 +0200

    copy of Task's fields and setters/getters

--- src/model/Context.cpp
+++ src/model/Context.cpp
@@ -11,3 +11,21 @@ void Context::execute(){
 void Context::changeState(const std::shared_ptr<State> &s) {
     state_ = s;
 }
+
+//void Context::setTask(const Task &t){
+//    task_ = t;
+//}
+//
+//Task Context::getTask() const{
+//    return task_;
+//}
+
+void Context::setTitle(const std::string &title){
+    title_ = title;
+}
+void Context::setDueDate(const std::string & dd){
+    due_date_ = dd;
+}
+void Context::setPriority(const Task::Priority &p){
+    priority_ = p;
+}
--- src/model/Context.h
+++ src/model/Context.h
@@ -9,15 +9,28 @@
 #include <iostream>
 
 #include "State.h"
+#include "TaskManager.h"
 
 class Context{
 public:
     virtual void execute();
     void changeState(const std::shared_ptr<State> &);
     std::shared_ptr<State> getState () { return state_; };
+//    void setTask(const Task &);
+//    Task getTask() const;
+    void setTitle(const std::string &);
+    void setDueDate(const std::string &);
+    void setPriority(const Task::Priority &);
 
 private:
+    friend class AddTaskState;
+    friend class ShowState;
     std::shared_ptr<State> state_;
+//    Task task_ = Task{};
+    std::string title_;
+    std::string due_date_;
+    Task::Priority priority_;
+    std::shared_ptr<TaskManager> man_ = std::shared_ptr<TaskManager>{new TaskManager};
 };
 
 #endif //TASKMANAGER_SRC_MODEL_CONTEXT_H_
