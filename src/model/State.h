b2e4de48c70df65305e7f4b49791a1f97010a95d
Anton Ovcharenko <antonovch@icloud.com>
2021-11-24 19:28:38 +0200

    removed classname method

--- src/model/State.h
+++ src/model/State.h
@@ -18,7 +18,6 @@ class State{
 public:
     virtual void execute(Context &c, StateFactory &f) = 0;
     static std::string readline(const std::string &prompt);
-    virtual std::string classname() const { return "State"; }
 
 protected:
     void changeState(const std::shared_ptr<Context> &, std::shared_ptr<State>);
@@ -26,51 +25,39 @@ protected:
 
 class HomeState : public State{
     void execute(Context &c, StateFactory &f) override;
-    std::string classname() const override { return "HomeState"; }
 };
 
 class HelpState : public State{
     void execute(Context &c, StateFactory &f) override;
-    std::string classname() const override { return "HelpState"; }
 };
 
 class AddState : public State{
     void execute(Context &c, StateFactory &f) override;
-    std::string classname() const override { return "AddState"; }
 
 };
 
 class ReadTitleState : public State{
     void execute(Context &c, StateFactory &f) override;
-    std::string classname() const override { return "ReadTitleState"; }
-
 };
 
 class ReadPriorityState : public State{
     void execute(Context &c, StateFactory &f) override;
-    std::string classname() const override { return "ReadPriorityState"; }
-
 };
 
 class ReadDueDateState : public State{
     void execute(Context &c, StateFactory &f) override;
-    std::string classname() const override { return "ReadDueDateState"; }
-
 };
 
 class QuitState : public State{
     void execute(Context &c, StateFactory &f) override;
-    std::string classname() const override { return "QuitState"; }
 };
 
 class AddTaskState : public State{
     void execute(Context &c, StateFactory &f) override;
-    std::string classname() const override { return "AddTaskState"; }
 };
 
 class ShowState : public State{
     void execute(Context &c, StateFactory &f) override;
-    std::string classname() const override { return "ShowState"; }
 };
 
 #endif //TASKMANAGER_SRC_MODEL_STATE_H_
