5f8955042e226ab7226d7115f560c3ed3299cc26
Anton Ovcharenko <antonovch@icloud.com>
2021-11-26 13:37:49 +0200

    lazy initialization fix

--- src/model/StateFactory.h
+++ src/model/StateFactory.h
@@ -8,13 +8,35 @@
 #include <memory>
 #include <string>
 #include <map>
-//#include "State.h"
 
-class State;
+#include "State.h"
 
 class StateFactory{
 public:
-    std::shared_ptr<State> create(const std::string &) ;
+    StateFactory();
+
+    std::shared_ptr<State> create(const std::string &);
+
+    std::shared_ptr<State> nextStep();
+    std::shared_ptr<State> nextStep(const HelpState &);
+    std::shared_ptr<State> nextStep(const AddState &);
+    std::shared_ptr<State> nextStep(const ReadTitleState &);
+    std::shared_ptr<State> nextStep(const ReadPriorityState &);
+    std::shared_ptr<State> nextStep(const ReadDueDateState &);
+    std::shared_ptr<State> nextStep(const QuitState &);
+    std::shared_ptr<State> nextStep(const AddTaskState &);
+    std::shared_ptr<State> nextStep(const ShowState &);
+
+    std::shared_ptr<State> getHomeState();
+    std::shared_ptr<State> getHelpState();
+    std::shared_ptr<State> getAddState();
+    std::shared_ptr<State> getReadTitleState();
+    std::shared_ptr<State> getReadPriorityState();
+    std::shared_ptr<State> getReadDueDateState();
+    std::shared_ptr<State> getQuitState();
+    std::shared_ptr<State> getAddTaskState();
+    std::shared_ptr<State> getShowState();
+
 private:
     std::map<std::string, std::shared_ptr<State>> steps_;
 };
