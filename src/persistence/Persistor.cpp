b46f9fc7838d1eb24696a14425ab8b49fadb2e41
Anton Ovcharenko <antonovch@icloud.com>
2021-12-16 23:32:07 +0200

    filling of children vector

--- src/persistence/Persistor.cpp
+++ src/persistence/Persistor.cpp
@@ -11,10 +11,8 @@ void Persistor::save(const TaskManager &tm, std::ostream &os) {
     google::protobuf::util::SerializeDelimitedToOstream(tm.gen()->state(), &os);
     for (const auto &kv : tm.getTasks()) {
         ProtoTask::TaskEntity te;
-        auto id = new ProtoTask::TaskID(kv.first);
-        te.set_allocated_id(id);
-        auto task = new ProtoTask::Task(kv.second.first);
-        te.set_allocated_data(task);
+        te.set_allocated_id(new ProtoTask::TaskID(kv.first));
+        te.set_allocated_data(new ProtoTask::Task(kv.second.first));
         if (kv.second.second.parent()) {
             auto parent = new ProtoTask::TaskID(*kv.second.second.parent());
             te.set_allocated_parent(parent);
@@ -31,14 +29,18 @@ TaskManager Persistor::load(std::istream &is) {
 
     std::map<ProtoTask::TaskID, std::pair<ProtoTask::Task, Node>> tasks;
     ProtoTask::TaskEntity te;
-    Node n;
     while (!clean_eof) {
         google::protobuf::util::ParseDelimitedFromZeroCopyStream(&te, &iis, &clean_eof);
+        Node n;
         if (te.has_parent())
             n.SetParent(te.parent());
         tasks.insert(std::make_pair(te.id(),
                                     std::make_pair(te.data(), n)));
     }
+
+    for (const auto &kv : tasks)
+        if (kv.second.second.parent())
+            tasks[*kv.second.second.parent()].second.AddChild(kv.first);
 
     return {std::make_shared<IDGenerator>(state), tasks};
 }
