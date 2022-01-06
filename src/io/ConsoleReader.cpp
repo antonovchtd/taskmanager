5bef4d905436fe1ab90adcced6b5e1e1ca1659dd
Anton Ovcharenko <anton.ovcharenko@teamdev.com>
2022-01-06 13:48:56 +0200

    added input string trimming

--- src/io/ConsoleReader.cpp
+++ src/io/ConsoleReader.cpp
@@ -4,9 +4,19 @@
 
 #include "ConsoleReader.h"
 
+static inline void trim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
 std::string ConsoleReader::read(const std::string &prompt) {
     std::cout << prompt;
     std::string input;
     getline(std::cin, input);
+    trim(input);
     return input;
 }
