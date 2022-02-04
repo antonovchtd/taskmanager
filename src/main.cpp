034a9297ef879959fdbce6430d8bca029db46151
Anton Ovcharenko <anton.ovcharenko@teamdev.com>
2022-02-04 15:34:31 +0200

    added client-side logs

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,27 @@
  */
 
 #include "ui/Machine.h"
+#include "logging/Logger.h"
 #include "transfer/TaskManagerClient.h"
 
 int main() {
+    auto logger = Logger()
+            .SetFilename("taskmanager_client.log")
+            .PrintToConsole(false)
+            .SetFilterLevel(logging::trivial::info);
+
+    logger.init();
 
     std::string target_str = "localhost:50051";
     auto model = std::make_shared<TaskManagerClient>(
             grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+
+//    src::severity_logger<logging::trivial::severity_level> lg;
+    LOG_STREAM(info) << "Configured connection to " << target_str << ".";
+
     Machine m{model};
+
+    LOG_STREAM(info) << "Running state machine...";
     m.run();
     return 0;
 }
